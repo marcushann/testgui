314f2fd1e36e5fac8b20acfb2f61c1b2ee08a0c6
Marcus Hann <marcushann@logisticsmod.co.uk>
2015-02-08 20:42:47 +0000

    implement saving

--- definately_not_rocket_science/MainWindow.cs
+++ definately_not_rocket_science/MainWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using Gtk;
+using System.IO;
 
 public partial class MainWindow: Gtk.Window
 {	
@@ -27,5 +28,13 @@ public partial class MainWindow: Gtk.Window
 	protected void OnButtonLowerClicked (object sender, EventArgs e)
 	{
 		textview1.Buffer.Text = textview1.Buffer.Text.ToLower ();
+	}
+
+	protected void OnButtonSaveClicked (object sender, EventArgs e)
+	{
+		StreamWriter sw = new StreamWriter ("Test.txt");
+		sw.Write (textview1.Buffer.Text);
+		textview1.Buffer.Text = "Saved To File!";
+		sw.Close ();
 	}
 }
