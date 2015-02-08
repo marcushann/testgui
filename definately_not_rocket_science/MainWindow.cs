8ecce8bf5b9c897194f2c6a35c8f72654983532a
Marcus Hann <marcushann@logisticsmod.co.uk>
2015-02-08 21:07:48 +0000

    setup basic gui layout

--- definately_not_rocket_science/MainWindow.cs
+++ definately_not_rocket_science/MainWindow.cs
@@ -15,27 +15,4 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 
-	protected void OnButtonClearClicked (object sender, EventArgs e)
-	{
-		textview1.Buffer.Text = "";
-		//comment
-	}
-
-	protected void OnButtonUpperClicked (object sender, EventArgs e)
-	{
-		textview1.Buffer.Text = textview1.Buffer.Text.ToUpper ();
-	}
-
-	protected void OnButtonLowerClicked (object sender, EventArgs e)
-	{
-		textview1.Buffer.Text = textview1.Buffer.Text.ToLower ();
-	}
-
-	protected void OnButtonSaveClicked (object sender, EventArgs e)
-	{
-		StreamWriter sw = new StreamWriter ("Test.txt");
-		sw.Write (textview1.Buffer.Text);
-		textview1.Buffer.Text = "Saved To File!";
-		sw.Close ();
-	}
 }
